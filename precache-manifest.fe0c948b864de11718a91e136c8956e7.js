31a01f31ebd911b1f78636987c122288a7c5aeed
miszczu-blady <mateusz.sikora@tooploox.com>
2020-03-27 15:38:40 +0100

    Updates

---
+++ precache-manifest.fe0c948b864de11718a91e136c8956e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79fa39d759a3e31b64be3e0f3db5d4f5",
+    "url": "/crisis-stats/index.html"
+  },
+  {
+    "revision": "dac7c844b9c71aa35060",
+    "url": "/crisis-stats/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "9b88736af8383ed1f208",
+    "url": "/crisis-stats/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dac7c844b9c71aa35060",
+    "url": "/crisis-stats/static/js/2.e6167bf8.chunk.js"
+  },
+  {
+    "revision": "81b6e0f34fc5a80a94fad82949fb96e7",
+    "url": "/crisis-stats/static/js/2.e6167bf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b88736af8383ed1f208",
+    "url": "/crisis-stats/static/js/main.0e1c151a.chunk.js"
+  },
+  {
+    "revision": "cee770a1c45727277c92",
+    "url": "/crisis-stats/static/js/runtime-main.0fecac41.js"
+  }
+]);
