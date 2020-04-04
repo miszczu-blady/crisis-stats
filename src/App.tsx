f617c7fbad2a39b15a626d87c3cc12095d7a7b07
Mateusz <mateusz.sikora@tivix.com>
2020-04-04 21:25:13 +0200

    hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+import React, { FC } from 'react';
+import { HashRouter } from "react-router-dom";
 
 import { MainPageWithRouter } from './app/components/pages/main';
 
 import './App.css';
 import 'antd/dist/antd.css'
 
-
-function App() {
-  return (
-  	<Router basename="/crisis-stats/">
-    	<MainPageWithRouter />
-    </Router>
-  );
-}
+const App: FC = () => (
+  <HashRouter>
+    <MainPageWithRouter />
+  </HashRouter>
+)
 
 export default App;
