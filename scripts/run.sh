4820aed703ce617802e2910c0d6ca67dd67edcef
Mateusz <mateusz.sikora@tivix.com>
2020-04-08 20:13:28 +0200

    data update

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,8 @@
-python otodom.py --transaction=wynajem --property-type=mieszkanie --city=gdansk --page-from=1 --page-to=40
-python otodom.py --transaction=sprzedaz --property-type=mieszkanie --city=gdansk --page-from=1 --page-to=40
-python otodom.py --transaction=wynajem --property-type=mieszkanie --city=krakow --page-from=1 --page-to=40
-python otodom.py --transaction=sprzedaz --property-type=mieszkanie --city=krakow --page-from=1 --page-to=40
-python otodom.py --transaction=wynajem --property-type=mieszkanie --city=warszawa --page-from=1 --page-to=40
-python otodom.py --transaction=sprzedaz --property-type=mieszkanie --city=warszawa --page-from=1 --page-to=40
-python otodom.py --transaction=wynajem --property-type=mieszkanie --city=wroclaw --page-from=1 --page-to=40
-python otodom.py --transaction=sprzedaz --property-type=mieszkanie --city=wroclaw --page-from=1 --page-to=40
+python otodom.py --transaction=wynajem --property-type=mieszkanie --city=gdansk --page-from=1 --page-to=50
+python otodom.py --transaction=sprzedaz --property-type=mieszkanie --city=gdansk --page-from=1 --page-to=50
+python otodom.py --transaction=wynajem --property-type=mieszkanie --city=krakow --page-from=1 --page-to=50
+python otodom.py --transaction=sprzedaz --property-type=mieszkanie --city=krakow --page-from=1 --page-to=60
+python otodom.py --transaction=wynajem --property-type=mieszkanie --city=warszawa --page-from=1 --page-to=60
+python otodom.py --transaction=sprzedaz --property-type=mieszkanie --city=warszawa --page-from=1 --page-to=60
+python otodom.py --transaction=wynajem --property-type=mieszkanie --city=wroclaw --page-from=1 --page-to=60
+python otodom.py --transaction=sprzedaz --property-type=mieszkanie --city=wroclaw --page-from=1 --page-to=60
